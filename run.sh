ddc4a5933628f037e9a8acfa3eb73efa59468422
Sonya Castro <sonya.castro.gomez@gmail.com>
2023-09-24 20:51:28 -0500

    feat: comands

--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@
 # chmod u+x run.sh
 
 # from lex to c
-flex -o LAB01.yy.c LAB01.l
+flex -o LAB01.yy.cpp LAB01.l
 
 # from c to out
-gcc LAB01.yy.c -o LAB01.out
+g++ -std=c++11 -o LAB01.out LAB01.yy.cpp -lfl
 
 # run the file from txt
 ./LAB01.out entrada.txt > salida.txt
+
+cat salida.txt
+echo "\n"
